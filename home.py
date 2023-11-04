02c8a18341ee14c5fe419a24cf3aade87770fe1f
Khurram Rashid <98824765+KhurramRashid6893@users.noreply.github.com>
2023-11-04 19:33:25 +0530

    home.py

--- home.py
+++ home.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from PIL import Image
+
 
 def app():
-	st.markdown("<h1 style='text-align: center; color: red;'>Student Performance web App</h1>", unsafe_allow_html=True)
+	st.markdown("<h1 style='text-align: center; color: red;'>Student Performance Web App</h1>", unsafe_allow_html=True)
 	
 	st.markdown("<h1 style='text-align: center; color: blue;'>Google Developer Student Clubs</h1>", unsafe_allow_html=True)
 	
