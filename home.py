0bd9089e2eb31d23bb5067b5c4627177fc40c186
Khurram Rashid <98824765+KhurramRashid6893@users.noreply.github.com>
2023-11-04 19:16:19 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import Image
+
+def app():
+	st.markdown("<h1 style='text-align: center; color: red;'>Student Performance web App</h1>", unsafe_allow_html=True)
+	
+	st.markdown("<h1 style='text-align: center; color: blue;'>Google Developer Student Clubs</h1>", unsafe_allow_html=True)
+	
+	
+	
+     
